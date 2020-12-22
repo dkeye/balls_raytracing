1db1592164ea921f328fa56b60d4ce5dd2011eed
dk <ldifmo@gmail.com>
2020-12-23 01:17:51 +0300

    v2.2; some optimize

--- main.py
+++ main.py
@@ -3,29 +3,26 @@ from __future__ import annotations
 import time
 from functools import lru_cache
 from math import inf, sqrt
-from typing import NewType, NamedTuple, Union, Optional
+from typing import NamedTuple, Union, Optional
 
 from PIL import Image, ImageFont
 from PIL.ImageDraw import Draw
 
-version = 2.1
-
-ACoord = NewType("ACoord", int)
-BCoord = NewType("BCoord", Union[int, float])
+version = 2.2
 
 
 class Vector(NamedTuple):
-    x: BCoord
-    y: BCoord
-    z: BCoord
+    x: Union[int, float]
+    y: Union[int, float]
+    z: Union[int, float]
 
     def __mul__(self, other: Vector) -> int:
         """Dot product of two 3D vectors."""
-        return sum(BCoord(c1 * c2) for c1, c2 in zip(self, other))
+        return self[0] * other[0] + self[1] * other[1] + self[2] * other[2]
 
     def __pow__(self, power: Union[float, int]) -> Vector:
         """Computes k * vec"""
-        return Vector(*tuple(BCoord(c1 * power) for c1 in self))
+        return Vector(self[0] * power, self[1] * power, self[2] * power)
 
     def __rpow__(self, power):
         """Computes k * vec"""
@@ -33,11 +30,11 @@ class Vector(NamedTuple):
 
     def __sub__(self, other: Vector) -> Vector:
         """Computes v1 - v2"""
-        return Vector(*tuple(BCoord(c1 - c2) for c1, c2 in zip(self, other)))
+        return Vector(self[0] - other[0], self[1] - other[1], self[2] - other[2])
 
     def __add__(self, other: Vector) -> Vector:
         """Computes v1 + v2"""
-        return Vector(*tuple(BCoord(c1 + c2) for c1, c2 in zip(self, other)))
+        return Vector(self[0] + other[0], self[1] + other[1], self[2] + other[2])
 
     def __len__(self):
         """Length of a 3D vector."""
@@ -67,21 +64,21 @@ canvas_height: int = 800
 
 viewport_size: int = 1
 projection_plane_z: int = 1
-camera_position: Vector = Vector(BCoord(0), BCoord(0), BCoord(0))
+camera_position: Vector = Vector(0, 0, 0)
 
 BACKGROUND_COLOR: tuple = (255, 255, 255)
 
 spheres = [
-    Sphere(Vector(BCoord(0), BCoord(-1), BCoord(3)), 1, (255, 0, 0)),
-    Sphere(Vector(BCoord(2), BCoord(0), BCoord(4)), 1, (0, 0, 255)),
-    Sphere(Vector(BCoord(-2), BCoord(0), BCoord(4)), 1, (0, 255, 0)),
-    Sphere(Vector(BCoord(0), BCoord(-5001), BCoord(0)), 5000, (255, 255, 0)),
+    Sphere(Vector(0, -1, 3), 1, (255, 0, 0)),
+    Sphere(Vector(2, 0, 4), 1, (0, 0, 255)),
+    Sphere(Vector(-2, 0, 4), 1, (0, 255, 0)),
+    Sphere(Vector(0, -5001, 0), 5000, (255, 255, 0)),
 ]
 
 lights = [
     Light(type="ambient", intensity=0.2),
-    Light(type="point", intensity=0.6, position=Vector(BCoord(2), BCoord(1), BCoord(0))),
-    Light(type="directional", intensity=0.2, direction=Vector(BCoord(1), BCoord(4), BCoord(4))),
+    Light(type="point", intensity=0.6, position=Vector(2, 1, 0)),
+    Light(type="directional", intensity=0.2, direction=Vector(1, 4, 4)),
 ]
 
 img = Image.new(mode="RGB",
@@ -109,7 +106,7 @@ draw = Draw(img)
 #
 
 @lru_cache(maxsize=canvas_height * canvas_width)
-def encode(x: ACoord, y: ACoord) -> (BCoord, BCoord):
+def encode(x: int, y: int) -> (int, int):
     """
     A to B
     width = 600
@@ -117,11 +114,11 @@ def encode(x: ACoord, y: ACoord) -> (BCoord, BCoord):
     encode(0, 0) == (-300, 200)
     encode(600, 400) == (300, -200)
     """
-    return BCoord(-canvas_width // 2 + x), BCoord(canvas_height // 2 - y)
+    return -canvas_width // 2 + x, canvas_height // 2 - y
 
 
 @lru_cache(maxsize=canvas_height * canvas_width)
-def decode(x: BCoord, y: BCoord) -> (ACoord, ACoord):
+def decode(x: int, y: int) -> (int, int):
     """
     B to A
     width = 600
@@ -129,25 +126,25 @@ def decode(x: BCoord, y: BCoord) -> (ACoord, ACoord):
     decode(-300, 200) == (0, 0)
     decode(300, -200) == (600, 400)
     """
-    return ACoord(canvas_width // 2 + x), ACoord(canvas_height // 2 - y - 1)
+    return canvas_width // 2 + x, canvas_height // 2 - y - 1
 
 
 # precompute
 for xt in range(canvas_width):
     for yt in range(canvas_height):
-        ACoord(xt), ACoord(yt)
-        xtt, ytt = encode(ACoord(xt), ACoord(yt))
+        xtt, ytt = encode(xt, yt)
         decode(xtt, ytt)
 
 
-def put_pixel(x: BCoord, y: BCoord, color: str, draw: Draw = draw) -> None:
+def put_pixel(x: int, y: int, color: str, draw: Draw = draw) -> None:
     x, y = decode(x, y)
     if not (x < 0 or x > canvas_width or y < 0 or y > canvas_height):
+        color = int(color[0]), int(color[1]), int(color[2])
         draw.point(xy=(x, y), fill=color)
 
 
-def canvas_to_viewport(x: BCoord, y: BCoord, d: BCoord = BCoord(projection_plane_z)) -> Vector:
-    return Vector(BCoord(x * viewport_size / canvas_width), BCoord(y * viewport_size / canvas_height), d)
+def canvas_to_viewport(x: int, y: int, d: int = projection_plane_z) -> Vector:
+    return Vector(x * viewport_size / canvas_width, y * viewport_size / canvas_height, d)
 
 
 def compute_lighting(point: Vector, normal: Vector) -> float:
@@ -179,6 +176,7 @@ def intersect_ray_sphere(origin: Vector, direction_ray: Vector, sphere: Sphere) 
     discriminant: int = k2 * k2 - 4 * k1 * k3
     if discriminant < 0:
         return inf, inf
+
     t1: float = (-k2 + sqrt(discriminant)) / (2 * k1)
     t2: float = (-k2 - sqrt(discriminant)) / (2 * k1)
     return t1, t2
@@ -203,16 +201,14 @@ def trace_ray(origin: Vector, direction_ray: Vector, t_min: float, t_max: float)
     normal: Vector = (1 / normal.__len__()) ** normal
 
     cl = compute_lighting(point, normal)
-    return tuple(map(lambda color: int(cl * color), closest_sphere.color))
+    return cl * closest_sphere.color[0], cl * closest_sphere.color[1], cl * closest_sphere.color[2]
 
 
 def main():
     start = time.time()
 
     for x in range(-canvas_width // 2, canvas_width // 2):
-        x = BCoord(x)
         for y in range(-canvas_height // 2, canvas_height // 2):
-            y = BCoord(y)
             direction_ray = canvas_to_viewport(x, y)
             color = trace_ray(camera_position, direction_ray, 1, inf)
             if color != BACKGROUND_COLOR:
@@ -234,4 +230,4 @@ def main():
 if __name__ == '__main__':
     main()
     img.show()
-    img.save(f'v{version}.png', "png", compress_level=0)
+    # img.save(f'v{version}.png', "png", compress_level=0)
